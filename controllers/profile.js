82fafdae312b1684429039018b53ac24d922192f
unknown <samatth16@gmail.com>
2021-04-23 10:48:27 -0400

    adding JWT/REDIS

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
   db.select('*').from('users').where({id})
@@ -11,6 +13,23 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  const {name, age, pet} = req.body.formInput;
+  db('users')
+    .where({ id })
+    .update({ name })
+    .then( resp => {
+      if (resp) {
+        res.json("success")
+      } else{
+        res.status(400).json('unable to update')
+      }
+    })
+    .catch(err => res.status(400).json('error updating user'))
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
